861dfd8a7a54889c80e066bed824603e9e569356
Vinicius Rolim Barbosa <vinirb@outlook.com.br>
2021-07-15 20:22:31 -0400

    Organizando..

--- Atacado - CREATE TABLE subcategoria.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE subcategoria (
-	subcatid INT NOT NULL IDENTITY(1,1),
-	catid INT NOT NULL,
-	descricao VARCHAR(MAX) NOT NULL,
-	datainsert DATETIME NOT NULL DEFAULT GETDATE(),
-	CONSTRAINT pk_subcategoria PRIMARY KEY (subcatid),
-	CONSTRAINT fk_subcategoria_categoria FOREIGN KEY (catid) REFERENCES categoria(catid)
-)
-GO
